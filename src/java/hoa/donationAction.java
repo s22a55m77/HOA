4e28be5a33ae13c009236906020cd14db2eff5ca
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2022-11-26 10:54:48 +0800

    add donation

--- src/java/hoa/donationAction.java
+++ src/java/hoa/donationAction.java
@@ -5,10 +5,30 @@
  */
 package hoa;
 
+import java.util.*;
+import java.sql.*;
+
 /**
  *
  * @author ccslearner
  */
 public class donationAction {
+    
+    
+    public void record(int assetID, String donor_completename, String donor_address,
+                       double amount, int acceptingOfficer, String donorsform_filename,
+                       String donation_date, int authorizing_president) {
+        donation donation = new donation();
+        donation.assetID = assetID;
+        donation.donor_completename = donor_completename;
+        donation.donor_address = donor_address;
+        donation.amount = amount;
+        donation.acceptingOfficer = acceptingOfficer;
+        donation.donorsform_filename = donorsform_filename;
+        donation.donation_date = donation_date;
+        donation.authorizing_president = authorizing_president;
+        
+        donation.addDonation();
+    }
     
 }
